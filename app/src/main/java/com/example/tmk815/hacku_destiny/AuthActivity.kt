99cd4b9a7d8c1bcda7038da68871807dedc7ea41
門田朋己 <monndenn15@gmail.com>
2019-06-13 13:18:48 +0900

    AndroidXへの置換、認証ボタンの設置

--- app/src/main/java/com/example/tmk815/hacku_destiny/AuthActivity.kt
+++ app/src/main/java/com/example/tmk815/hacku_destiny/AuthActivity.kt
@@ -1,6 +1,6 @@
 package com.example.tmk815.hacku_destiny
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 class AuthActivity : AppCompatActivity() {
 }
--- app/src/main/java/com/example/tmk815/hacku_destiny/MainActivity.kt
+++ app/src/main/java/com/example/tmk815/hacku_destiny/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.tmk815.hacku_destiny
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
