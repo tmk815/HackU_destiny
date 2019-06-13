163f33f5c2479d05234ad928e2fbdc7702af6756
門田朋己 <monndenn15@gmail.com>
2019-06-13 17:43:37 +0900

    Login処理を追記

--- app/src/main/java/com/example/tmk815/hacku_destiny/AuthActivity.kt
+++ app/src/main/java/com/example/tmk815/hacku_destiny/AuthActivity.kt
@@ -1,6 +1,70 @@
 package com.example.tmk815.hacku_destiny
 
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_auth.*
+import java.util.*
 
 class AuthActivity : AppCompatActivity() {
+    companion object {
+        const val RC_SIGN_IN = 123
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+        login.setOnClickListener {
+            val providers: List<AuthUI.IdpConfig> = Arrays.asList(
+                AuthUI.IdpConfig.GoogleBuilder().build()
+            )
+
+// Create and launch sign-in intent
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build(),
+                RC_SIGN_IN
+            )
+        }
+
+        /*logOut.setOnClickListener {
+            AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener {
+                    Log.d("signOut","サインアウトしました")
+                }
+        }*/
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                Log.d("UID", user?.uid)
+                val settingIntent = Intent(this, MainActivity::class.java)
+                startActivity(settingIntent)
+                finish()
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                Log.d("LoginError", response?.error?.errorCode.toString())
+            }
+        }
+    }
 }
