f5832b5bc527fc21d41b84061b7ff515e45a27d2
門田朋己 <monndenn15@gmail.com>
2019-06-28 00:44:23 +0900

    科目名、講義回数、選択画像の表示機能を追加

--- app/src/main/java/com/example/tmk815/hacku_destiny/PostActivity.kt
+++ app/src/main/java/com/example/tmk815/hacku_destiny/PostActivity.kt
@@ -1,13 +1,23 @@
 package com.example.tmk815.hacku_destiny
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
-
+import kotlinx.android.synthetic.main.activity_post.*
+import java.io.IOException
 
 
 class PostActivity : AppCompatActivity() {
+
+    companion object {
+        const val READ_REQUEST_CODE = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +27,30 @@ class PostActivity : AppCompatActivity() {
         val database = FirebaseDatabase.getInstance()
         val myRef = database.getReference("post").database.getReference("中部大学").database.getReference(currentUser!!.uid)
 
+        note_picture_button.setOnClickListener {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+            intent.addCategory(Intent.CATEGORY_OPENABLE)
+
+            intent.type = "image/*"
+            startActivityForResult(intent, READ_REQUEST_CODE)
+        }
+
+    }
+
+    @SuppressLint("MissingSuperCall")
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            var uri: Uri? = null
+            if (resultData != null) {
+                uri = resultData.data
+                try {
+                    val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+                    notePicture.setImageBitmap(bitmap)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
+            }
+        }
     }
 }
